c92f68f112395c3c6664e0d13b65947235607747
Jonathan Kim <jkimbo@gmail.com>
2014-10-29 16:25:02 +0000

    Move search result out

---
+++ dist/search-result.js
@@ -0,0 +1,50 @@
+/** @jsx React.DOM */
+"use strict";
+
+var React = require('react/addons');
+var cx = React.addons.classSet;
+
+var TextHighlight = require('./text-highlight');
+
+//
+// Search result
+//
+var SearchResult = React.createClass({displayName: 'SearchResult',
+  propTypes: {
+    selected: React.PropTypes.bool,
+    onHover: React.PropTypes.func.isRequired,
+    onClick: React.PropTypes.func.isRequired,
+    label: React.PropTypes.string.isRequired,
+    option: React.PropTypes.object.isRequired,
+    tokens: React.PropTypes.array.isRequired,
+  },
+
+  shouldComponentUpdate: function(nextProps, nextState) {
+    if (this.props.tokens !== nextProps.tokens) {
+      return true;
+    }
+
+    if (this.props.selected !== nextProps.selected) {
+      return true;
+    }
+
+    return false;
+  },
+
+  render: function() {
+    var classes = cx({
+      'select-result': true,
+      'selected': !!this.props.selected
+    });
+
+    return (
+      React.DOM.li({className: classes, 
+        onMouseEnter: this.props.onHover.bind(null, this.props.option), 
+        onMouseDown: this.props.onClick.bind(null, this.props.option)}, 
+        TextHighlight({text: this.props.label, tokens: this.props.tokens})
+      )
+    );
+  }
+});
+
+module.exports = SearchResult;
--- dist/single.js
+++ dist/single.js
@@ -6,48 +6,7 @@ var _ = require('lodash');
 var Sifter = require('sifter');
 var cx = React.addons.classSet;
 
-var TextHighlight = require('./text-highlight');
-
-//
-// Search result
-//
-var SearchResult = React.createClass({displayName: 'SearchResult',
-  propTypes: {
-    selected: React.PropTypes.bool,
-    onHover: React.PropTypes.func.isRequired,
-    onClick: React.PropTypes.func.isRequired,
-    label: React.PropTypes.string.isRequired,
-    option: React.PropTypes.object.isRequired,
-    tokens: React.PropTypes.array.isRequired,
-  },
-
-  shouldComponentUpdate: function(nextProps, nextState) {
-    if (this.props.tokens !== nextProps.tokens) {
-      return true;
-    }
-
-    if (this.props.selected !== nextProps.selected) {
-      return true;
-    }
-
-    return false;
-  },
-
-  render: function() {
-    var classes = cx({
-      'select-result': true,
-      'selected': !!this.props.selected
-    });
-
-    return (
-      React.DOM.li({className: classes, 
-        onMouseEnter: this.props.onHover.bind(null, this.props.option), 
-        onMouseDown: this.props.onClick.bind(null, this.props.option)}, 
-        TextHighlight({text: this.props.label, tokens: this.props.tokens})
-      )
-    );
-  }
-});
+var SearchResult = require('./search-result');
 
 //
 // Auto complete select box
