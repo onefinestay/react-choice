688c1741d45333f8bab9b5509d18aff66eea8df0
Jonathan Kim <jkimbo@gmail.com>
2015-05-12 16:43:08 +0100

    Move to new dev build

--- example/index.jsx
+++ example/index.jsx
@@ -34,62 +34,50 @@ var Index = React.createClass({
     });
 
     return (
-      <html>
-        <head>
-          <title>React Choice Demo</title>
-          <link href='//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.0/styles/docco.min.css' rel='stylesheet' type='text/css'></link>
-          <link href="css/index.css" rel="stylesheet"></link>
-          <link href="css/choice/index.css" rel="stylesheet"></link>
-        </head>
-        <body>
-          <Header />
-          <GithubRibbon />
+      <main id="app">
+        <Header />
+        <GithubRibbon />
 
-          <div className="content">
-            <div className="examples">
-              <div className="example-single">
-                <div className="example">
-                  <h2>Single Choice</h2>
-                  <Choice.Select placeholder="Select a country">
-                    {options}
-                  </Choice.Select>
+        <div className="content">
+          <div className="examples">
+            <div className="example-single">
+              <div className="example">
+                <h2>Single Choice</h2>
+                <Choice.Select placeholder="Select a country">
+                  {options}
+                </Choice.Select>
 
-                  <CodeSnippet language="javascript" toggle={false}>
-                    {singleExample}
-                  </CodeSnippet>
-                </div>
+                <CodeSnippet language="javascript" toggle={false}>
+                  {singleExample}
+                </CodeSnippet>
               </div>
-
-              <div className="example-multiple">
-                <div className="example">
-                  <h2>Multiple Choice</h2>
-                  <Choice.SelectMultiple placeholder="Select a country">
-                    {options}
-                  </Choice.SelectMultiple>
-
-                  <CodeSnippet language="javascript" toggle={false}>
-                    {multipleExample}
-                  </CodeSnippet>
-                </div>
-              </div>
-              <div style={{clear: 'both'}}></div>
             </div>
 
-            <Features />
-            <Install />
-            <div className="tutorials">
-              <h2>Tutorials</h2>
-              <CustomRender />
+            <div className="example-multiple">
+              <div className="example">
+                <h2>Multiple Choice</h2>
+                <Choice.SelectMultiple placeholder="Select a country">
+                  {options}
+                </Choice.SelectMultiple>
+
+                <CodeSnippet language="javascript" toggle={false}>
+                  {multipleExample}
+                </CodeSnippet>
+              </div>
             </div>
+            <div style={{clear: 'both'}}></div>
           </div>
 
-          <Footer />
+          <Features />
+          <Install />
+          <div className="tutorials">
+            <h2>Tutorials</h2>
+            <CustomRender />
+          </div>
+        </div>
 
-          <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.0/highlight.min.js" charSet="utf-8"></script>
-          <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.0/languages/javascript.min.js" charSet="utf-8"></script>
-          <script src="build/index.js"></script>
-        </body>
-      </html>
+        <Footer />
+      </main>
     );
   }
 });
